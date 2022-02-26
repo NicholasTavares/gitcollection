dd4c39bf77123c898b37ace2d5cb2badf659e3f4
NicholasTavares <nicholasbalby@hotmail.com>
2022-02-26 15:54:33 -0300

    Estilizacao do dashboard - parte 1

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Title, Form } from './styles';
+import logo from '../../assets/logo.svg'
+
+export const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios do Github</Title>
+      <Form>
+        <input type="text" placeholder='username/repository_name' />
+        <button type='submit'>Buscar</button>
+      </Form>
+    </>
+  )
+}
