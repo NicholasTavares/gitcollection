1e5b1e34d80f7374b9bd08f6388251da3738d603
NicholasTavares <nicholasbalby@hotmail.com>
2022-02-27 17:35:09 -0300

    feat: estilizacao parte 2

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Title, Form } from './styles';
+import {FiChevronRight} from 'react-icons/fi'
+import { Title, Form, Repos } from './styles';
 import logo from '../../assets/logo.svg'
 
 export const Dashboard: React.FC = () => {
@@ -11,6 +12,18 @@ export const Dashboard: React.FC = () => {
         <input type="text" placeholder='username/repository_name' />
         <button type='submit'>Buscar</button>
       </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img src="https://avatars.githubusercontent.com/u/48931095?v=4" alt="Repositório" />
+          <div>
+            <strong>NicholasTavares/mini-curso-reactjs</strong>
+            <p>Repositório do mini curso gratuito de reactjs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+      </Repos>
     </>
   )
 }
